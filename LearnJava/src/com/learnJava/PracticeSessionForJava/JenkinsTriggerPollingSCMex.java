0cb861a2c5ddd4c97ecf4e8a195f2edf2d97081a
Vignesh Jayapal <vigneshjayapal7@gmail.com>
2026-04-03 13:29:35 +0530

    code push for new implementation

---
+++ LearnJava/src/com/learnJava/PracticeSessionForJava/JenkinsTriggerPollingSCMex.java
@@ -0,0 +1,7 @@
+package com.learnJava.PracticeSessionForJava;
+
+public class JenkinsTriggerPollingSCMex {
+    public static void main(String[]args){
+        System.out.println("Sample Project!!!");
+    }
+}
